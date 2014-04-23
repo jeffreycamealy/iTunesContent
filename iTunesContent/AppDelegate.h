22bb3ff3190a7de5afc289db1ce7f323f493d741
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-04-22 20:38:12 -0500

    Initial Commit

---
+++ iTunesContent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunesContent
+//
+//  Created by Jeffrey Camealy on 4/22/14.
+//  Copyright (c) 2014 bearMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
