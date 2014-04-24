f0a25f1ceb7aba8513cf5e40d25276a8bc7a84db
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-04-24 11:33:21 -0500

    Build initial drag and snap

--- iTunesContent/SourceCode/PodcastsListVC.m
+++ iTunesContent/SourceCode/PodcastsListVC.m
@@ -12,12 +12,12 @@
 #import "Podcast.h"
 #import "Card.h"
 
-@interface PodcastsListVC () <UITableViewDataSource, UITableViewDelegate>
+@interface PodcastsListVC ()
 {
-    UITableView *tableView;
     NSArray *podcasts;
     UIDynamicAnimator *animator;
     UISnapBehavior *snapBehavior;
+    UIAttachmentBehavior *attachmentBehavior;
     Card *card;
 }
 @end
@@ -30,15 +30,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-    tableView.dataSource = self;
-    tableView.delegate = self;
-    [self.view addSubview:tableView];
-    [self loadPodcasts];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    [self loadPodcasts];
     [self addCard];
     
+    // Animator
+    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    // Tap Recognizer
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
     [self.view addGestureRecognizer:tapRecognizer];
 }
@@ -49,9 +49,8 @@
 - (void)loadPodcasts {
     Server *server = [Server new];
     [[server getTopPodcasts]
-     subscribeNext:^(id x) {
-         podcasts = x;
-         [tableView reloadData];
+     subscribeNext:^(NSArray *somePodcasts) {
+         podcasts = somePodcasts;
      } error:^(NSError *error) {
          // TODO: handle error
      }];
@@ -61,10 +60,23 @@
     card = [[Card alloc] initWithFrame:CGRectMake(10, 10, 200, 300)];
     [self.view addSubview:card];
     
-    card.backgroundColor = [UIColor grayColor];
-    
+    // Pan Recognizer
     UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(cardPanned:)];
     [card addGestureRecognizer:panRecognizer];
+    
+    // Swipe Recognizer
+    UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(cardSwiped:)];
+    swipeRecognizer.direction = UISwipeGestureRecognizerDirectionLeft | UISwipeGestureRecognizerDirectionRight;
+    [card addGestureRecognizer:swipeRecognizer];
+    
+    
+    //----------------
+    UIOffset attachmentPoint = UIOffsetMake(-25.0, -25.0);
+    // By default, an attachment behavior uses the center of a view. By using a
+    // small offset, we get a more interesting effect which will cause the view
+    // to have rotation movement when dragging the attachment.
+    attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:card offsetFromCenter:attachmentPoint attachedToAnchor:CGPointMake(100, 100)];
+    [animator addBehavior:attachmentBehavior];
 }
 
 - (void)viewTapped:(UITapGestureRecognizer *)tapRecognizer {
@@ -94,46 +106,161 @@
 #pragma mark - Gesture Recognizer
 
 - (void)cardPanned:(UIPanGestureRecognizer *)panRecognizer {
-//    CGPoint translation = [panRecognizer translationInView:self.view];
-//    panRecognizer.view.center = CGPointMake(panRecognizer.view.center.x + translation.x,
-//                                         panRecognizer.view.center.y + translation.y);
-//    [panRecognizer setTranslation:CGPointMake(0, 0) inView:self.view];
-    
-//    UISnapBehavior *snapBehavior = [[UISnapBehavior alloc] initWithItem:panRecognizer.view snapToPoint:CGPointMake(0, 0)];
-//    [animator addBehavior:snapBehavior];
-//    
-//    UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[panRecognizer.view]];
-//    itemBehavior.elasticity = 0.5;
-//    [animator addBehavior:itemBehavior];
-}
-
-#pragma mark - Tableview DataSource
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return podcasts.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    switch (panRecognizer.state) {
+        case UIGestureRecognizerStateBegan: {
+            CGPoint touchPoint = [panRecognizer locationInView:panRecognizer.view];
+            UIOffset cardOffset = UIOffsetMake(touchPoint.x - panRecognizer.view.bounds.size.width/2.0,
+                                               touchPoint.y - panRecognizer.view.bounds.size.height/2.0);
+            CGPoint anchor = [panRecognizer locationInView:self.view];
+            
+            attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:panRecognizer.view
+                                                           offsetFromCenter:cardOffset
+                                                           attachedToAnchor:anchor];
+            
+            [animator addBehavior:attachmentBehavior];
+        } break;
+            
+        case UIGestureRecognizerStateChanged: {
+            CGPoint anchor = [panRecognizer locationInView:self.view];
+            attachmentBehavior.anchorPoint = anchor;
+        } break;
+            
+        case UIGestureRecognizerStateEnded: {
+            [animator removeBehavior:attachmentBehavior];
+            [self snapToPoint];
+        } break;
+            
+        default:
+            break;
     }
-    
-    Podcast *podcast = podcasts[indexPath.row];
-    
-    cell.textLabel.text = podcast.title;
-    cell.detailTextLabel.text = podcast.itunesPath;
-    
-    return cell;
 }
 
+- (void)snapToPoint {
+    CGPoint point = CGPointMake(10, 586/2);
+    
+    card.transform = CGAffineTransformIdentity;
+    // Remove the previous behavior.
+    [animator removeBehavior:snapBehavior];
+    
+    snapBehavior = [[UISnapBehavior alloc] initWithItem:card snapToPoint:point];
+    __weak UIView *weakCard = card;
+    
+    const int maxVariance = 10;
+    int r = rand()%(maxVariance*2);
+    r -= maxVariance;
+    r = r ?: r+1; // Make sure it's not zero;
+    snapBehavior.action = ^{
+        NSLog(@"%i", r);
+        weakCard.transform = CGAffineTransformRotate(weakCard.transform, M_PI/100.0/(float)r);
+    };
+    [animator addBehavior:snapBehavior];
+}
 
-#pragma mark - Tableview Delegate
+- (void)cardPannedd:(UIPanGestureRecognizer *)gesture
+{
+    static UIAttachmentBehavior *attachment;
+    static CGPoint               startCenter;
+    
+    // variables for calculating angular velocity
+    
+    static CFAbsoluteTime        lastTime;
+    static CGFloat               lastAngle;
+    static CGFloat               angularVelocity;
+    
+    if (gesture.state == UIGestureRecognizerStateBegan)
+    {
+        [animator removeAllBehaviors];
+        
+        startCenter = gesture.view.center;
+        
+        // calculate the center offset and anchor point
+        
+        CGPoint pointWithinAnimatedView = [gesture locationInView:gesture.view];
+        
+        UIOffset offset = UIOffsetMake(pointWithinAnimatedView.x - gesture.view.bounds.size.width / 2.0,
+                                       pointWithinAnimatedView.y - gesture.view.bounds.size.height / 2.0);
+        
+        CGPoint anchor = [gesture locationInView:gesture.view.superview];
+        
+        // create attachment behavior
+        
+        attachment = [[UIAttachmentBehavior alloc] initWithItem:gesture.view
+                                               offsetFromCenter:offset
+                                               attachedToAnchor:anchor];
+        
+        // code to calculate angular velocity (seems curious that I have to calculate this myself, but I can if I have to)
+        
+        lastTime = CFAbsoluteTimeGetCurrent();
+        lastAngle = [self angleOfView:gesture.view];
+        
+        attachment.action = ^{
+            CFAbsoluteTime time = CFAbsoluteTimeGetCurrent();
+            CGFloat angle = [self angleOfView:gesture.view];
+            if (time > lastTime) {
+                angularVelocity = (angle - lastAngle) / (time - lastTime);
+                lastTime = time;
+                lastAngle = angle;
+            }
+        };
+        
+        // add attachment behavior
+        
+        [animator addBehavior:attachment];
+    }
+    else if (gesture.state == UIGestureRecognizerStateChanged)
+    {
+        // as user makes gesture, update attachment behavior's anchor point, achieving drag 'n' rotate
+        
+        CGPoint anchor = [gesture locationInView:gesture.view.superview];
+        attachment.anchorPoint = anchor;
+    }
+    else if (gesture.state == UIGestureRecognizerStateEnded)
+    {
+        [animator removeAllBehaviors];
+        
+        CGPoint velocity = [gesture velocityInView:gesture.view.superview];
+        
+        // if we aren't dragging it down, just snap it back and quit
+        
+        if (fabs(atan2(velocity.y, velocity.x) - M_PI_2) > M_PI_4) {
+            UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:gesture.view snapToPoint:startCenter];
+            [animator addBehavior:snap];
+            
+            return;
+        }
+        
+        // otherwise, create UIDynamicItemBehavior that carries on animation from where the gesture left off (notably linear and angular velocity)
+        
+        UIDynamicItemBehavior *dynamic = [[UIDynamicItemBehavior alloc] initWithItems:@[gesture.view]];
+        [dynamic addLinearVelocity:velocity forItem:gesture.view];
+        [dynamic addAngularVelocity:angularVelocity forItem:gesture.view];
+        [dynamic setAngularResistance:2];
+        
+        // when the view no longer intersects with its superview, go ahead and remove it
+        
+        dynamic.action = ^{
+            if (!CGRectIntersectsRect(gesture.view.superview.bounds, gesture.view.frame)) {
+                [animator removeAllBehaviors];
+                [gesture.view removeFromSuperview];
+                
+                [[[UIAlertView alloc] initWithTitle:nil message:@"View is gone!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+            }
+        };
+        [animator addBehavior:dynamic];
+        
+        // add a little gravity so it accelerates off the screen (in case user gesture was slow)
+        
+        UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:@[gesture.view]];
+        gravity.magnitude = 0.7;
+        [animator addBehavior:gravity];
+    }
+}
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    Podcast *podcast = podcasts[indexPath.row];
-    [UIApplication.sharedApplication openURL:[NSURL URLWithString:podcast.itunesPath]];
+- (CGFloat)angleOfView:(UIView *)view
+{
+    // http://stackoverflow.com/a/2051861/1271826
+    
+    return atan2(view.transform.b, view.transform.a);
 }
 
 @end
